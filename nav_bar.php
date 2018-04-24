b86a0d2fd4c08500e7ca258c3af55bf3208861b9
lizzie1486 <hc3gf@virginia.edu>
2018-04-24 00:29:41 -0400

    Added recipes under search hits

--- nav_bar.php
+++ nav_bar.php
@@ -1,4 +1,4 @@
-<nav class="navbar navbar-default">
+<nav class="navbar navbar-inverse">
 	<div class="container-fluid">
 		<!-- Brand and toggle get grouped for better mobile display -->
 		<div class="navbar-header">
@@ -8,7 +8,7 @@
 				<span class="icon-bar"></span>
 				<span class="icon-bar"></span>
 			</button>
-			<a class="navbar-brand" href="index.php">Noshspoon</a>
+			<a class="navbar-brand" href="index.php"><i class="glyphicon glyphicon-ice-lolly-tasted"></i>Noshspoon</a>
 		</div>
 		<!-- Collect the nav links, forms, and other content for toggling -->
 		<div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
@@ -16,37 +16,44 @@
 				<div class="input-group">
 					<input type="text" class="form-control" id="search" name="search" placeholder="Search">
 					<div class="input-group-btn">
-						<button type="submit" class="btn btn-primary"><i class="glyphicon glyphicon-search"></i></button>
+						<button type="submit" class="btn btn-danger"><i class="glyphicon glyphicon-search"></i></button>
 					</div>
 				</div>
 			</form>
 			<ul class="nav navbar-nav navbar-right">
 				<?php if(isset($_SESSION['usertype']) && strcmp($_SESSION['usertype'], 'admin') == 0) { ?>
-					<li><a href="new_item.php">Post Item for Sale</a></li>
+					<li class="dropdown"><a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false"><i class="glyphicon glyphicon-th-large"></i> Item<span class="caret"></span></a>
+						<ul class="dropdown-menu">
+							<li><a href="new_item.php"><i class="glyphicon glyphicon-plus-sign"></i> Add New Item</a></li>
+							<li><a href="#"><i class="glyphicon glyphicon-remove-sign"></i> Delete Item</a></li>
+						</ul>
+					</li>
+				<?php } 
+				if(true){ ?>
+					<li class="dropdown"><a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false"><i class="glyphicon glyphicon-shopping-cart"></i> Cart<span class="caret"></span></a>
+						<ul class="dropdown-menu">
+							<li><a href="cart.php"><i class="glyphicon glyphicon-zoom-in"></i> View Cart</a></li>
+							<li><a href="check_out.php"><i class="glyphicon glyphicon-ok-circle"></i> Check Out</a></li>
+						</ul>
+					</li>
 				<?php } ?>
-				<li class="dropdown"><a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Cart<span class="caret"></span></a>
-					<ul class="dropdown-menu">
-						<li><a href="cart.php">View Cart</a></li>
-						<li><a href="check_out.php">Check Out</a></li>
-					</ul>
-				</li>
 				<li class="dropdown">
 					<a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">
 						<?php
 						if (isset($_SESSION['username'])){
 							$username = $_SESSION['username'];
-							printf('%s <span class="caret"></span></a>
+							printf('<i class="glyphicon glyphicon-user"></i> %s <span class="caret"></span></a>
 								<ul class="dropdown-menu">
-								<li><a href="my_listings.php">My Listings</a></li>
-								<li><a href="my_orders.php">My Orders</a></li>
+								<!--<li><a href="my_listings.php">My Listings</a></li>-->
+								<li><a href="my_orders.php"><i class="glyphicon glyphicon-list-alt"></i> My Orders</a></li>
 								<li role="separator" class="divider"></li>
-								<li><a href="log_out.php">Logout</a></li>
+								<li><a href="log_out.php"><i class="glyphicon glyphicon-log-out"></i> Logout</a></li>
 								</ul>', $username);
 						} else {
 							echo 'Account' . ' <span class="caret"></span></a>
 								<ul class="dropdown-menu">
-								<li><a href="" data-toggle="modal" data-target="#login-overlay">Login</a></li>
-								<li><a href="" data-toggle="modal" data-target="#login-overlay">Register</a></li>
+								<li><a href="" data-toggle="modal" data-target="#login-overlay"><i class="glyphicon glyphicon-log-in"></i> Login</a></li>
+								<li><a href="" data-toggle="modal" data-target="#login-overlay"><i class="glyphicon glyphicon-new-window"></i> Register</a></li>
 								</ul>';
 							}
 						?>
--- search.php
+++ search.php
@@ -26,9 +26,26 @@
 		if (!$result) {
 			die($connection->error);
 		} elseif ($result->num_rows) {
+			$recipes = array();
+
 			echo '<div class="container">';
 
 			while ($row = $result->fetch_assoc()) {
+				//Code to search for recipes that include any of the items in query as ingredient
+				$ind = $row['item_id'];
+
+				//Queries for any recipes that include the items hit from the search as ingredients or any recipes that also match the search keyword
+				$subquery = "SELECT * FROM Recipe NATURAL JOIN Ingredients WHERE item_id=$ind OR (UCASE(food_name) LIKE UCASE('%$search%') OR UCASE(description) LIKE UCASE('%$search%'))";
+				$subresult = $connection->query($subquery);
+				if(!$subresult){
+					die($connection->error);
+				}
+				elseif($subresult->num_rows){
+					while($ingred = $subresult->fetch_assoc()){
+						array_push($recipes, $ingred);
+					}
+				}
+
 				if($row['available']) {
 				printf('
 				<div class="col-sm-6 col-md-4">
@@ -43,29 +60,67 @@
 				</div>
 				</div>
 				</div>', $row['picture'], $row['item_name'], $row['description'], $row['item_id'], $row['item_id'], $row['price']);
+				}
+				else {
+					printf('
+					<div class="col-sm-6 col-md-4">
+					<div class="thumbnail">
+					<img src="%s" alt="...">
+					<div class="caption">
+					<h3>%s</h3>
+					<p>%s</p>
+					<p><a href="#" class="btn btn-danger" >SOLD OUT</a>	&nbsp; <a href="review.php?item_id=%d" class="btn btn-primary" role="button">Reviews</a> &nbsp;  $%.2lf </p>
+					</div>
+					</div>
+					</div>', $row['picture'], $row['item_name'], $row['description'], $row['item_id'], $row['item_id'], $row['price']);
+				}
 			}
-			else {
-				printf('
-				<div class="col-sm-6 col-md-4">
+
+			foreach($recipes as $rec){
+				printf('<div class="col-sm-6 col-md-4">
 				<div class="thumbnail">
 				<img src="%s" alt="...">
 				<div class="caption">
 				<h3>%s</h3>
 				<p>%s</p>
-				<p><a href="#" class="btn btn-danger" >SOLD OUT</a>	&nbsp; <a href="review.php?item_id=%d" class="btn btn-primary" role="button">Reviews</a> &nbsp;  $%.2lf </p>
+
 				</div>
 				</div>
-				</div>', $row['picture'], $row['item_name'], $row['description'], $row['item_id'], $row['item_id'], $row['price']);
-			}
+				</div>', $rec['food_pic'], $rec['food_name'], $rec['description']);
 			}
 			echo '</div>';
-		} else {
-			echo '<div class="alert alert-danger" role="alert"><button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button>No results found!</div>'; //no result, stay on the search page, echo this
+		}
+		else {
+			//Goes through just the Recipes to see if any search keywords matched
+			$query = "SELECT * FROM Recipe WHERE (UCASE(food_name) LIKE UCASE('%$search%') 
+			OR UCASE(description) LIKE UCASE('%$search%'))";
+			$result = $connection->query($query);
+			if (!$result) {
+				die($connection->error);
+			} elseif ($result->num_rows) {
+				echo '<div class="container">';
+				while ($row = $result->fetch_assoc()) {
+					printf('<div class="col-sm-6 col-md-4">
+					<div class="thumbnail">
+					<img src="%s" alt="...">
+					<div class="caption">
+					<h3>%s</h3>
+					<p>%s</p>
+
+					</div>
+					</div>
+					</div>', $row['food_pic'], $row['food_name'], $row['description']);
+				}
+			}
+			else{
+				echo '<div class="alert alert-danger" role="alert"><button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button>No results found!</div>'; //no result, stay on the search page, echo this
+			}
 		}
 	} else {
 		echo '<p>Invalid search field, please try again.</p>';
 	}
 	mysqli_close($connection);
+	?>
 	?>
 
 
